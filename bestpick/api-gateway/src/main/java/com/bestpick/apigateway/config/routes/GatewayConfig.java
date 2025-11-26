d602de317b7c9733c37d2dad0ffc52f19ba5c788
Manu <manuel.delossantos@dialapplet.com>
2025-11-26 14:29:17 +0100

    feat: added social graph to api gateway

--- bestpick/api-gateway/src/main/java/com/bestpick/apigateway/config/routes/GatewayConfig.java
+++ bestpick/api-gateway/src/main/java/com/bestpick/apigateway/config/routes/GatewayConfig.java
@@ -10,35 +10,41 @@ import org.springframework.http.HttpMethod;
 @Configuration
 public class GatewayConfig {
 
-    @Autowired
-    private JWTAuthenticationFilter jwtAuthenticationFilter;
+        @Autowired
+        private JWTAuthenticationFilter jwtAuthenticationFilter;
 
-    @Autowired
-    private ApiKeyFilter apiKeyFilter;
+        @Autowired
+        private ApiKeyFilter apiKeyFilter;
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/api/users")
-                        .and()
-                        .method(HttpMethod.POST)
-                        .filters(f -> f.filter(apiKeyFilter))
-                        .uri("lb://users-microservice"))
+        @Bean
+        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route(r -> r.path("/api/users")
+                                                .and()
+                                                .method(HttpMethod.POST)
+                                                .filters(f -> f.filter(apiKeyFilter))
+                                                .uri("lb://users-microservice"))
 
-                .route(r -> r.path("/api/users/**")
-                        .filters(f -> f.filter(jwtAuthenticationFilter))
-                        .uri("lb://users-microservice"))
+                                .route(r -> r.path("/api/users/**")
+                                                .filters(f -> f.filter(jwtAuthenticationFilter))
+                                                .uri("lb://users-microservice"))
 
-                .route(r -> r.path("/eureka/web")
-                        .filters(f -> f.setPath("/"))
-                        .uri("http://localhost:8761"))
+                                .route(r -> r.path("/api/social-graph")
+                                                .and()
+                                                .method(HttpMethod.POST)
+                                                .filters(f -> f.filter(apiKeyFilter))
+                                                .uri("lb://social-graph-microservice"))
 
-                .route(r -> r.path("/eureka/**")
-                        .uri("http://localhost:8761"))
+                                .route(r -> r.path("/eureka/web")
+                                                .filters(f -> f.setPath("/"))
+                                                .uri("http://localhost:8761"))
 
-                .route(r -> r.path("/auth/**")
-                        .uri("lb://auth-microservice"))
+                                .route(r -> r.path("/eureka/**")
+                                                .uri("http://localhost:8761"))
 
-                .build();
-    }
+                                .route(r -> r.path("/auth/**")
+                                                .uri("lb://auth-microservice"))
+
+                                .build();
+        }
 }
