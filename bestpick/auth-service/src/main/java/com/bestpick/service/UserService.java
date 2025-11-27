3712af12a103bc102fbdc170e82c20c04d20792f
Manu <manuel.delossantos@dialapplet.com>
2025-11-27 16:27:18 +0100

    chore: unused import

--- bestpick/auth-service/src/main/java/com/bestpick/service/UserService.java
+++ bestpick/auth-service/src/main/java/com/bestpick/service/UserService.java
@@ -1,7 +1,5 @@
 package com.bestpick.service;
 
-import java.sql.Array;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
