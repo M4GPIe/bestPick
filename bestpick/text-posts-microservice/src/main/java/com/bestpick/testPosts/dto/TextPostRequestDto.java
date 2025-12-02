9fd85b3be2eeb7fa9f36fbf2568d5c3b07745712
Manu <manuel.delossantos@dialapplet.com>
2025-12-02 22:19:35 +0100

    fix: metadata should be added in creation

--- bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/dto/TextPostRequestDto.java
+++ bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/dto/TextPostRequestDto.java
@@ -1,11 +1,7 @@
 package com.bestpick.testPosts.dto;
 
-import com.bestpick.testPosts.model.PostMetadata;
-
 public record TextPostRequestDto(
                 String userId,
-                String postBody,
-                String timestamp,
-                PostMetadata postMetadata) {
+                String postBody) {
 
 }
