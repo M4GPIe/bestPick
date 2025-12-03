3fb1d3a2bd46ba6f8122a35c163e857fbd4ff816
Manu <manuel.delossantos@dialapplet.com>
2025-12-03 09:52:20 +0100

    feat: post textpost

--- bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/model/PostMetadata.java
+++ bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/model/PostMetadata.java
@@ -4,11 +4,9 @@ import java.time.Instant;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class PostMetadata {
     private Instant creationDate;
 
--- bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/service/TextPostService.java
+++ bestpick/text-posts-microservice/src/main/java/com/bestpick/testPosts/service/TextPostService.java
@@ -1,6 +1,10 @@
 package com.bestpick.testPosts.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,7 +12,11 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.bestpick.testPosts.dto.TextPostDto;
 import com.bestpick.testPosts.dto.TextPostRequestDto;
+import com.bestpick.testPosts.model.PostMetadata;
+import com.bestpick.testPosts.model.TextPost;
 import com.bestpick.testPosts.repository.TextPostRepository;
+
+import java.time.Instant;
 
 public class TextPostService {
 
@@ -16,8 +24,10 @@ public class TextPostService {
     TextPostRepository textPostRepository;
 
     public List<TextPostDto> getTextPosts(String userId, String[] hashTags) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTextPosts'");
+
+        List<TextPost> textPosts = textPostRepository.findByUserIdAndHashtags(userId, List.of(hashTags));
+
+        return textPosts.stream().map(TextPost::toDto).toList();
     }
 
     public void deletePost(String id) {
@@ -28,8 +38,35 @@ public class TextPostService {
     }
 
     public TextPostDto createTextPost(TextPostRequestDto post) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createTextPost'");
+
+        Instant now = Instant.now();
+
+        PostMetadata metadata = new PostMetadata(now, now);
+
+        Map<String, Integer> hashtags = findHashtagsInText(post.postBody());
+
+        TextPost newPost = new TextPost(null, post.userId(), post.postBody(), metadata, hashtags);
+
+        TextPost savedPost = textPostRepository.save(newPost);
+
+        return TextPost.toDto(savedPost);
+
+    }
+
+    private Map<String, Integer> findHashtagsInText(String text) {
+        Map<String, Integer> hashtagCounter = new HashMap<>();
+
+        Pattern HASHTAG_PATTERN = Pattern.compile("#(\\S+)");
+
+        Matcher matcher = HASHTAG_PATTERN.matcher(text);
+
+        while (matcher.find()) {
+            String hashtag = matcher.group(1);
+
+            hashtagCounter.merge(hashtag, 1, Integer::sum);
+        }
+
+        return hashtagCounter;
     }
 
     public TextPostDto updateTextPost(String id, TextPostRequestDto post) {
