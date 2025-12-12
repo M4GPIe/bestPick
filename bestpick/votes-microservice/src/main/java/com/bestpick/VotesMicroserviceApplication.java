43a73a9aea9559330a3e61ff179dca2f50363009
Manu <manuel.delossantos@dialapplet.com>
2025-12-12 09:58:41 +0100

    chore: created votes ms module

---
+++ bestpick/votes-microservice/src/main/java/com/bestpick/VotesMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package com.bestpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotesMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VotesMicroserviceApplication.class, args);
+    }
+}
